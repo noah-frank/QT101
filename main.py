ffe9d55be449a124e592d25cc4dd46489ba30e90
noah-frank <noahfrank0001@gmail.com>
2026-03-14 20:34:04 -0400

    started video 4

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from datetime import datetime
+import pytz
+import threading
 
 def get_sp500_tickers():
     site= "https://en.wikipedia.org/wiki/List_of_S&P_500_companies"
@@ -14,17 +17,20 @@ def get_sp500_tickers():
     tickers = list(df[0].Symbol)
     return tickers
 
-tickers = get_sp500_tickers()
-
-def get_history(ticker, period_start, period_end, granularity="1d"):
+def get_history(ticker, period_start, period_end, granularity="1d", tries=0):
     import yfinance
+    try:
+        df = yfinance.Ticker(ticker).history(
+            start=period_start, 
+            end=period_end, 
+            interval=granularity, 
+            auto_adjust=True
+        ).reset_index()
+    except Exception as err:
+        if tries < 5:
+            return get_history(ticker, period_start, period_end, granularity, tries+1)
+        return pd.DataFrame()
 
-    df = yfinance.Ticker(ticker).history(
-        start=period_start, 
-        end=period_end, 
-        interval=granularity, 
-        auto_adjust=True
-    ).reset_index()
     df = df.rename(columns={
         "Date": "datetime", 
         "Open": "open",
@@ -44,23 +50,37 @@ def get_history(ticker, period_start, period_end, granularity="1d"):
     df = df.set_index("datetime", drop=True)
     return df
 
-from datetime import datetime
-import pytz
+
+def get_histories(tickers, period_starts, period_ends, granularity="1d"):
+    dfs = [None] * len(tickers)
+    def _helper(i):
+        print(tickers[i])
+        df = get_history(
+            tickers[i], 
+            period_starts[i], 
+            period_ends[i], 
+            granularity=granularity
+        )
+        dfs[i] = df
+    threads = [threading.Thread(target=_helper, args=(i,)) for i in range(len(tickers))]
+    [thread.start() for thread in threads]
+    # If we don't do this, the rest of the function 
+    # will execute before the threads are complete 
+    [thread.join() for thread in threads] 
+    tickers = [tickers[i] for i in range(len(tickers)) if not dfs[i].empty]
+    dfs = [df for df in dfs if not df.empty]
+    return tickers, dfs
+
+def get_tickers_dfs(start, end):
+    tickers = get_sp500_tickers()
+    starts = [start]*len(tickers)
+    ends = [end]*len(tickers)
+    tickers,dfs = get_histories(tickers, starts, ends, granularity="1d")
+    return tickers, {ticker:df for ticker,df in zip(tickers, dfs)}
+
 
 period_start = datetime(2016, 1, 1, tzinfo=pytz.utc)
 period_end = datetime(2026, 1, 1, tzinfo=pytz.utc)
 
-# print(period_start)
-# print(period_end)
-# get_history("MMM", period_start, period_end, "1d")
-
-i = 0
-
-for ticker in tickers:
-    df = get_history(ticker, period_start, period_end)
-    print(ticker, df)
-
-    i += 1
-
-    if i >= 5:
-        break
+tickers, ticker_dfs = get_tickers_dfs(period_start, period_end)
+print(ticker_dfs)
